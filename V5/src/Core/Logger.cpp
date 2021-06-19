10582af578b4c53a310ef234cabe5c704b18fea1
Paolo5150 <p.ferri1986@gmail.com>
2021-06-19 14:19:43 +0800

    Interfaces, console logging

--- V5/src/Core/Logger.cpp
+++ V5/src/Core/Logger.cpp
@@ -1,19 +1,43 @@
 #include <Core/Logger.h>
 #include "spdlog/sinks/stdout_color_sinks.h"
 #include "spdlog/sinks/basic_file_sink.h"
+#include "spdlog/sinks/basic_file_sink-inl.h"
+#include "spdlog/sinks/sink.h"
+#include <iostream>
+#include <chrono>
+#include <ctime>  
+#include <sstream>
 using namespace V5Core;
 
 std::shared_ptr<spdlog::logger> Logger::s_Logger;
+std::shared_ptr<spdlog::logger> Logger::s_V5Logger;
+std::shared_ptr<spdlog::logger> Logger::s_ConsoleLogger;
 
 void Logger::Init()
 {
-	s_Logger = spdlog::basic_logger_mt("V5", "V5Log.txt");
+
+	time_t t = time(0);   // get time now
+	struct tm * now = localtime(&t);
+
+	char buffer[80];
+	strftime(buffer, 80, "%d-%m-%Y_%H-%M-%S", now);
+	std::string fileName(buffer);
+
+	std::stringstream ss;
+	ss << "Logs\\V5Log_" << fileName << ".txt";
+
+	auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(ss.str());
+
+	s_Logger = std::make_shared<spdlog::logger>(spdlog::logger("Generic",file_sink));
+	s_V5Logger = std::make_shared<spdlog::logger>(spdlog::logger("V5Engine",file_sink));
+	s_ConsoleLogger = spdlog::stdout_color_mt("Console");
 
 	spdlog::set_pattern("[%H:%M:%S] %v");
 
-	V5LOG_INFO("------------------ START OF LOG ----------------------\n");
+	s_Logger->info("------------------ START OF LOG ----------------------\n");
 
-	spdlog::set_pattern("[%H:%M:%S] [%t] %^%l%$: %v");
 	s_Logger->set_level(spdlog::level::trace);
+	spdlog::set_pattern("[%H:%M:%S] [%t] %^%l%$: %v");
+
 	
 }
--- V5/src/include/V5/Core/Logger.h
+++ V5/src/include/V5/Core/Logger.h
@@ -11,10 +11,16 @@ namespace V5Core
 	{
 	public:
 		static void Init();
-		inline static std::shared_ptr<spdlog::logger>& GetLogger() { return s_Logger; }
+		inline static std::shared_ptr<spdlog::logger>& GetLogger() {  return s_Logger; }
+		inline static std::shared_ptr<spdlog::logger>& GetV5Logger() { return s_V5Logger; }
+		inline static std::shared_ptr<spdlog::logger>& GetConsoleLogger() { return s_ConsoleLogger; }
+
+
 
 	private:
 		static std::shared_ptr<spdlog::logger> s_Logger;
+		static std::shared_ptr<spdlog::logger> s_V5Logger;
+		static std::shared_ptr<spdlog::logger> s_ConsoleLogger;
 	};
 
 #define V5LOG_TRACE(...)         ::V5Core::Logger::GetLogger()->trace(__VA_ARGS__)
@@ -22,4 +28,10 @@ namespace V5Core
 #define V5LOG_WARN(...)          ::V5Core::Logger::GetLogger()->warn(__VA_ARGS__)
 #define V5LOG_ERROR(...)         ::V5Core::Logger::GetLogger()->error(__VA_ARGS__)
 #define V5LOG_CRITICAL(...)      ::V5Core::Logger::GetLogger()->critical(__VA_ARGS__)
+
+#define V5CLOG_TRACE(...)         ::V5Core::Logger::GetConsoleLogger()->trace(__VA_ARGS__)
+#define V5CLOG_INFO(...)          ::V5Core::Logger::GetConsoleLogger()->info(__VA_ARGS__)
+#define V5CLOG_WARN(...)          ::V5Core::Logger::GetConsoleLogger()->warn(__VA_ARGS__)
+#define V5CLOG_ERROR(...)         ::V5Core::Logger::GetConsoleLogger()->error(__VA_ARGS__)
+#define V5CLOG_CRITICAL(...)      ::V5Core::Logger::GetConsoleLogger()->critical(__VA_ARGS__)
 }
