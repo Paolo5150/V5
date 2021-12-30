c901383dab34ca2d63965f226ca1dbf0c11b0591
Paolo <p.ferri1986@gmail.com>
2021-12-30 20:55:36 +0800

    Improving transform

--- V5/include/V5/Components/Components.h
+++ V5/include/V5/Components/Components.h
@@ -9,15 +9,29 @@ namespace V5Core
 	class Transform
 	{
 	public:
-		glm::vec3 Position;
-		glm::vec3 Scale;
-		glm::vec3 Rotation;
-
-		glm::mat4 GetMatrix() const
+		Transform()
 		{
-			auto rot = glm::toMat4(glm::quat(Rotation));
-		
-			return glm::translate(glm::mat4(1.0), Position) * rot * glm::scale(glm::mat4(1.0), Scale);
+			m_scale = glm::vec3(1, 1, 1 );
+			UpdateMatrix();
+		}
+
+		void SetPosition(const glm::vec3& p) { m_position = p; UpdateMatrix(); }
+
+		const glm::mat4& GetMatrix() const
+		{
+			return m_matrix;
+		}
+
+	private:
+
+		glm::vec3 m_position;
+		glm::vec3 m_scale;
+		glm::vec3 m_rotation;
+		glm::mat4 m_matrix;
+		void UpdateMatrix()
+		{
+			glm::mat4 rotation = glm::toMat4(glm::quat(m_rotation));
+			m_matrix = glm::translate(glm::mat4(1.0), m_position) * rotation * glm::scale(glm::mat4(1.0), m_scale);
 		}
 	};
 }
